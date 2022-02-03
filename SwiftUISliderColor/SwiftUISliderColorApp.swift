fb84e69db6f944411a420d074df78bb49f9f4e9f
B0Dim <bodim@me.com>
2022-02-03 18:40:48 +0300

    Initial Commit

---
+++ SwiftUISliderColor/SwiftUISliderColorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISliderColorApp.swift
+//  SwiftUISliderColor
+//
+//  Created by  BoDim on 03.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISliderColorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
